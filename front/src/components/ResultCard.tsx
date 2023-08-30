0ab0a7557b104df3a655f2b786af2f3635d01e27
최진 <gogleset@choejin-ui-MacBookPro.local>
2023-08-30 11:34:57 +0900

    styling: modal directory & Content, Home Component

--- front/src/components/ResultCard.tsx
+++ front/src/components/ResultCard.tsx
@@ -2,19 +2,19 @@
  * @gogleset 보여질 카드 섹션입니다.
  */
 
-import type { PlaceData } from "../Content";
+import type { PlaceData } from "../components/Content";
 
 const ResultCard: React.FC<{ data: PlaceData }> = ({ data }) => {
   console.log(data);
 
-  if (!data) return <>돌려돌려</>;
+  if (!data) return <div>조회 데이터가 없습니다.</div>;
 
   function onClickHandler(event: React.MouseEvent) {
     event.preventDefault();
     window.open(data.place_url);
   }
   return (
-    <div className='card w-96 bg-base-100 shadow-xl'>
+    <div className='card w-80 bg-base-100 shadow-xl'>
       <div className='card-body'>
         <h2 className='card-title'>{data.place_name}</h2>
         <p>{data.category_name}</p>
